aea95aa145da7744930cb24c078eaf7e08219496
Mutesi-Bridget <bridgettemutesi@gmail.com>
2023-03-10 07:35:38 +0300

    my first commit

---
+++ mywork.py
@@ -0,0 +1,48 @@
+Python 3.8.10 (default, Nov 14 2022, 12:59:47)
+[GCC 9.4.0] on linux
+Type "help", "copyright", "credits" or "license" for more information.
+>>> "Hello AkiraChix"
+'Hello AkiraChix'
+>>> 'Hello'
+'Hello'
+>>> "I an an learning python"
+'I an an learning python'
+>>> "I am learning python"
+'I am learning python'
+>>> School = "AkiraChix"
+>>> school = "Akirachix"
+>>> school
+'Akirachix'
+>>> type (school)
+<class 'str'>
+>>> "I love " + "Akirachix"
+'I love Akirachix'
+>>> "1"+ "2"
+'12'
+>>> "1"*"3"
+Traceback (most recent call last):
+  File "<stdin>", line 1, in <module>
+TypeError: can't multiply sequence by non-int of type 'str'
+>>> "1" * "3"
+Traceback (most recent call last):
+  File "<stdin>", line 1, in <module>
+TypeError: can't multiply sequence by non-int of type 'str'
+>>> "ha" *10
+'hahahahahahahahahaha'
+>>> "1"*3
+'111'
+>>> "ha" * "ha"
+Traceback (most recent call last):
+  File "<stdin>", line 1, in <module>
+TypeError: can't multiply sequence by non-int of type 'str'
+>>> print("I love Akirachix")
+I love Akirachix
+>>> print("Akirachix","is","a","good","school")
+Akirachix is a good school
+>>> print(2**2, "must be 4")
+4 must be 4
+>>> x =  "cat"
+>>> y = "dog"
+>>> print(F"{y} is better than{x}")
+dog is better thancat
+>>>
